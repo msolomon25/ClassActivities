8404be5a63a2e2ef001760baecc6fc525f4aae7c
Madori Solomon <madorisolomon@gmail.com>
2023-11-15 23:51:06 -0600

    Loops

---
+++ week2/2.3.5-Loops.js
@@ -0,0 +1,25 @@
+// Follow the steps below to create a **while** loop that TOTALS the numbers from 1 to 100
+
+// 1) create a variable to represent the current number
+
+let x = 1;
+
+// 2) create a variable to represent the current total
+
+let total = 0;
+
+// 3) write a while loop that sums the numbers from 1 to 100
+while (x <= 100){
+  total = total + x;
+
+  console.log(x);
+  x++;
+}
+
+// Now using the same variables you created in steps 1 and 2, create a **for** loops that TOTALS the numbers from 1 to 100
+
+// 1) write a for loop that sums the numbers from 1 to 100
+
+for (let x = 1; x <= 100; x++) {
+  console.log(x);
+}
